e4702f1c1625828f421f289072fafe1c3418ca86
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-02 11:58:30 -0400

    Add attributes test to movie spec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Movie do
   describe 'relationships' do
     it { should belong_to(:studio) }
   end
+
+  describe 'attributes' do
+    it 'has attributes' do
+      universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+      ark = universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+
+      expect(ark.title).to eq("Raiders of the Lost Ark")
+      expect(ark.creation_year).to eq(1981)
+      expect(ark.genre).to eq("Action/Adventure")
+    end
+  end
 end
