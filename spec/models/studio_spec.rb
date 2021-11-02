aa8ed9d523dcbc2927a579ade035686eb807aefc
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-02 11:56:13 -0400

    Add attributes test to studio spec

--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Studio do
   describe 'relationships' do
     it { should have_many(:movies) }
   end
+
+  describe 'attributes' do
+    it 'has attributes' do
+      universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+      mgm = Studio.create!(name: 'MGM', location: 'New York City')
+
+      expect(universal.name).to eq("Universal Studios")
+      expect(mgm.name).to eq("MGM")
+      expect(universal.location).to eq("Hollywood")
+      expect(mgm.location).to eq("New York City")
+    end
+  end
+
 end
